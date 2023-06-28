33363b23496b32357ba8e1feaa715f6e09c66d94
shawn <shwnmambo@gmail.com>
2023-06-28 05:34:34 -0700

    15:33

--- getfunction.c
+++ getfunction.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
+print_t print_funcs[] = {
+    {"p", print_add},
+    {"li", prinlint},
+    {"lu", prinlunt},
+    {"lo", prinloct},
+    {"lx", prinlhex},
+    {"lX", prinlupx},
+    {"hi", prinhint},
+    {"hu", prinhunt},
+    {"ho", prinhoct},
+    {"hx", prinhhex},
+    {"hX", prinhupx},
+    {"ti", prinpint},
+    {"to", prinnoct},
+    {"tx", prinnhex},
+    {"tX", prinnupx},
+    {"si", prinsint},
+    {NULL, NULL}
+};
+
+
 /**
  * get_print_func - selects the correct function to perform the operation.
  * @s: argument identifier

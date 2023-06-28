aabc52685ecc0693f50c2bb7740b1989f34d74f3
Just-andreew <andrewsolitei@gmail.com>
2023-06-28 03:53:08 -0700

    1352hrs

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _printf - Custom printf function.
- * @format: Format string.
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return: Number of characters printed.
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
@@ -13,58 +13,39 @@ int _printf(const char *format, ...)
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
 
-	va_start(arguments, format);
-	buffer = malloc(sizeof(char) * 1024);
-
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
 	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-
 	if (!format[i])
 		return (0);
-
 	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			{
-				print_buffer(buffer, ibuf);
-				free(buffer);
-				va_end(arguments);
+			{	print_buffer(buffer, ibuf), free(buffer), va_end(arguments);
 				return (-1);
 			}
 			else
-			{
-				function = get_print_function(format, i + 1);
+			{	function = get_print_func(format, i + 1);
 				if (function == NULL)
 				{
 					if (format[i + 1] == ' ' && !format[i + 2])
 						return (-1);
-					handle_buffer(buffer, format[i], ibuf);
-					len++;
-					i--;
+					handle_buffer(buffer, format[i], ibuf), len++, i--;
 				}
 				else
 				{
 					len += function(arguments, buffer, ibuf);
-					i += get_identifier_length(format, i + 1);
+					i += ev_print_func(format, i + 1);
 				}
-			}
-			i++;
+			} i++;
 		}
 		else
-		{
-			handle_buffer(buffer, format[i], ibuf);
-			len++;
-		}
-
+			handle_buffer(buffer, format[i], ibuf), len++;
 		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
 			;
 	}
-
-	print_buffer(buffer, ibuf);
-	free(buffer);
-	va_end(arguments);
+	print_buffer(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);
 }
-
